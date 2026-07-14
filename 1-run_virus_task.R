0cc22bad6c270fb187de5ee2d8ba7e0f3396378a
Russell Boag <rboag@outlook.com>
2026-07-14 16:33:51 +0200

    Clarify single-block run examples

--- 1-run_virus_task.R
+++ 1-run_virus_task.R
@@ -10,7 +10,7 @@ run_instructions()
 # Run task
 run_task()
 
-# # You can also run one block at a time by selecting a specific block:
+# You can also run one block at a time by selecting a specific block:
 # run_task(block = "CAL")
 # run_task(block = "MAN/PRE_AUTOMATION")
 # run_task(block = "REL_DROP")
